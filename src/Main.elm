a60658f8da31e19fb547139304153f3b9a6be842
Colin Hill <colin@peak2peakmedia.com>
2016-12-02 17:17:55 +0100

    [Fix] Main App: Fix the main app to use the new fetchAll Request in players

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,19 @@
 module Main exposing (..)
 
 import Html exposing (Html, div, text, program)
-import Messages exposing (Msg)
+import Messages exposing (Msg(..))
 import Models exposing (Model, initialModel)
 import Update exposing (update)
 import View exposing (view)
+import Players.Commands exposing (fetchAll)
+
+
+-- Now that we have a command and a valid message to send our user lets update
+-- init by showing the PlayersMsg (Error or Ok (JSON Status), and initialize the Fetch Command)
 
 init : ( Model, Cmd Msg )
 init =
-    ( initialModel, Cmd.none )
+    ( initialModel, Cmd.map PlayersMsg fetchAll )
 
 
 
--- src/Models.elm
+++ src/Models.elm
@@ -9,7 +9,7 @@ type alias Model =
 
 initialModel : Model
 initialModel =
-  -- this is a player constructor where Player
-  -- {(obj) (param1) (param2) (param3)}
-  { players = [ Player "1" "Sam" 1]
+  -- this is a player constructor where Player []
+  -- becomes a new player
+  { players = []
   }
